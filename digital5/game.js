b5d2e0ef1a26ff572df7fb2d7f4f872e6c5bb236
Eric <evelosky@gmu.edu>
2019-04-22 22:37:59 -0400

    update macaroni 2.0

--- digital5/game.js
+++ digital5/game.js
@@ -1,40 +1,37 @@
-/*
-* Example by Loonride - https://loonride.com/learn/phaser/p2-physics-bodies
-*/
-
 //set width and height variables for game
-var width = 1200;
-var height = 700;
+var width = 1600;
+var height = 900;
 //create game object and initialize the canvas
 var game = new Phaser.Game(width, height, Phaser.AUTO, null, {preload: preload, create: create, update: update});
 
 //initialize some variables
-var ball;
-var triangles;
-var ballSpeed = 250;
-var maxTriangleSpeed = 1500;
-var maxBallSpeed = 1000;
+var player;
+var noodles;
+var playerSpeed = 250;
+var maxnoodleSpeed = 1500;
+var maxplayerSpeed = 1000;
 var showBodies = false;
 var currentTime = 0;
 var timer;
-var gameDuration = 20;
+var gameDuration = 30;
+var prompt;
 
 function preload() {
 	//set background color of canvas
 	game.stage.backgroundColor = '#eee';
 
 	//load assets
-	game.load.image('ball', 'asset/ball.png');
-	game.load.image('triangle', 'asset/beach.png');
+	game.load.image('player', 'asset/macaroni.png');
+	game.load.image('noodle', 'asset/semicircle.png');
 
 	//load physics body polygon data from a local JSON file
 	this.game.load.physics("physics", "asset/data.json");
-
 }
 function create() {
-
-	//set world boundaries, allowing room at the bottom for triangles to fall
-	game.world.setBounds(0,0,width,height+100);
+	prompt = game.add.text(width*0.5, height*0.5, "Clear the Macaronis!!", {fontSize: "40px"});
+	prompt.anchor.set(0.5);
+	//set world boundaries, allowing room at the bottom for noodles to fall
+	game.world.setBounds(0,-100,width,height+50);
 
 	//start p2 physics engine
 	game.physics.startSystem(Phaser.Physics.P2JS);
@@ -42,25 +39,26 @@ function create() {
 	//initialize keyboard arrows for the game controls
 	cursors = game.input.keyboard.createCursorKeys();
 
-	//initialize ball
-	ball = game.add.sprite(width*0.5, height*0.8, "ball");
-	//ball.body.setCircle(45);
-	ball.scale.setTo(0.5,0.5);
-	game.physics.p2.enable(ball, showBodies);
+	//initialize player
+	player = game.add.sprite(width*0.5, height*0.8, "player");
+	//player.body.setCircle(45);
+	//player.scale.setTo(0.5,0.5);
+	game.physics.p2.enable(player, showBodies);
 	//add physics body polygon
-	ball.body.clearShapes();
-	//ball.body.loadPolygon("physics", "semicircle");
-	ball.body.setCircle(300);
-	//make ball kinematic so that it does not respond to collisions
-	ball.body.kinematic = true;
+	player.body.clearShapes();
+	player.body.loadPolygon("physics", "semicircle");
+	//player.body.setCircle(200);
+	//make player kinematic so that it does not respond to collisions
+	player.body.kinematic = true;
+	player.body.angle = 180;
 
-	//initialize triangle group
-	triangles = game.add.group();
+	//initialize noodle group
+	noodles = game.add.group();
 
-	//spawn a triangle every .8 seconds, unless the end of the game is near
+	//spawn a noodle every .8 seconds, unless the end of the game is near
 	game.time.events.loop(800, function() {
 		if (currentTime < gameDuration - 4) {
-			spawnTriangle();
+			spawnnoodle();
 		}
 	}, game);
 
@@ -68,85 +66,93 @@ function create() {
 	timer = game.add.text(10, 10, gameDuration, { fontSize: "24px"} );
 	//decrement the remaining time every second
 	game.time.events.loop(1000, updateTimer, game);
+
+}
+function render(){
+	//game.debug.body(player);
 }
 function update() {
-	if (ball.body.velocity.y < maxTriangleSpeed) {
-		ball.body.velocity.y+= 20;
+	if(currentTime == 1){
+		prompt.destroy();
+	}
+
+	if (player.body.velocity.y < maxnoodleSpeed) {
+		player.body.velocity.y+= 20;
 	}
 
 	//extra no leaving (makes walls "sticky")
-	if (ball.position.x < 0 || ball.position.x > width){
-		ball.body.velocity.x = 0;
+	if (player.position.x < 0 || player.position.x > width){
+		player.body.velocity.x = 0;
 	}
 
-	if(ball.position.y > height-250){ //ball is on the ground
-		ball.position.y = 400; //fix bad position???
-		ball.body.velocity.y = 0;
+	if(player.position.y > height-150){ //player is on the ground
+		player.position.y = 400; //fix bad position???
+		player.body.velocity.y = 0;
 		if(cursors.up.isDown){
-			ball.body.velocity.y -= 1000;
+			player.body.velocity.y -= 1000;
 		}
-		if (cursors.left.isDown && ball.position.x > 0) {
-			if(ball.body.velocity.x > 0){ // if it was going right
-				ball.body.velocity.x -= ballSpeed; //stop
+		if (cursors.left.isDown && player.position.x > 0) {
+			if(player.body.velocity.x > 0){ // if it was going right
+				player.body.velocity.x -= playerSpeed; //stop
 			}
-			if(ball.body.velocity.x > -maxBallSpeed){
-				ball.body.velocity.x -= ballSpeed/5; //accel
+			if(player.body.velocity.x > -maxplayerSpeed){
+				player.body.velocity.x -= playerSpeed/5; //accel
 			}
 		}
-		else if (cursors.right.isDown && ball.position.x < width) {
-			if(ball.body.velocity.x < 0){ // if it was going left
-				ball.body.velocity.x += ballSpeed; // stop
+		else if (cursors.right.isDown && player.position.x < width) {
+			if(player.body.velocity.x < 0){ // if it was going left
+				player.body.velocity.x += playerSpeed; // stop
 			}
-			if(ball.body.velocity.x < maxBallSpeed){
-				ball.body.velocity.x += ballSpeed/5; // accel
+			if(player.body.velocity.x < maxplayerSpeed){
+				player.body.velocity.x += playerSpeed/5; // accel
 			}
 		}
 		else {
-			ball.body.velocity.x = 0;
+			player.body.velocity.x = 0;
 		}
 	}
-	else { //ball is in the air
+	else { //player is in the air
 		if(cursors.down.isDown){
-			ball.body.velocity.y += 500;
+			player.body.velocity.y += 500;
 		}
-		if (cursors.left.isDown && ball.position.x > 0) {
-			ball.body.velocity.x -= ballSpeed/10;
+		if (cursors.left.isDown && player.position.x > 10) {
+			player.body.velocity.x -= playerSpeed/10;
 		}
-		else if (cursors.right.isDown && ball.position.x < width) {
-			ball.body.velocity.x += ballSpeed/10;
+		else if (cursors.right.isDown && player.position.x < width) {
+			player.body.velocity.x += playerSpeed/10;
 		}
 	}
-	//move the ball right and left based on keyboard arrows ^^
+	//move the player right and left based on keyboard arrows ^^
 
 
-	//iterate through the triangle group
-	for (var i in triangles.children) {
-		var triangle = triangles.children[i];
-		//accelerate triangles downards if below a maximum speed
-		if (triangle.body.velocity.y < maxTriangleSpeed) {
-			triangle.body.velocity.y+= 20;
+	//iterate through the noodle group
+	for (var i in noodles.children) {
+		var noodle = noodles.children[i];
+		//accelerate noodles downards if below a maximum speed
+		if (noodle.body.velocity.y < maxnoodleSpeed) {
+			noodle.body.velocity.y+= 20;
 		}
-		//destroy triangles that have left through the bottom of the viewport
-		if (triangle.position.y > height + 50) {
-			triangle.destroy();
+		//destroy noodles that have left through the top of the viewport
+		if (noodle.position.y < height*0.1) {
+			noodle.destroy();
 		}
 	}
 }
 
-function spawnTriangle() {
-	//spawn triangle randomly at the top of the screen
+function spawnnoodle() {
+	//spawn noodle randomly at the top of the screen
 	var x = Math.random() * width;
-	var triangle = game.add.sprite(x, height*0.1, "triangle");
-	game.physics.p2.enable(triangle, showBodies);
+	var noodle = game.add.sprite(x, height*0.1, "noodle");
+	game.physics.p2.enable(noodle, showBodies);
 	//add physics body polygon
-	triangle.body.clearShapes();
-	triangle.body.loadPolygon("physics", "triangle");
+	noodle.body.clearShapes();
+	noodle.body.loadPolygon("physics", "semicircle");
 	//traingle.body.setCircle(100);
-	//move triangle downwards
-	triangle.body.moveDown(0);
+	//move noodle downwards
+	noodle.body.moveDown(0);
 
-	//add triangle to the triangles group
-	triangles.add(triangle);
+	//add noodle to the noodles group
+	noodles.add(noodle);
 }
 
 function updateTimer() {
@@ -155,12 +161,18 @@ function updateTimer() {
 	timer.setText(gameDuration - currentTime);
 
 	/*
-	* stop the game and display the number of triangles caught
+	* stop the game and display the number of noodles caught
 	* when time has run out
 	*/
 	if (currentTime == gameDuration) {
-		var score = triangles.children.length;
-		var txt = game.add.text(width*0.5, height*0.5, score, {fontSize: "40px"});
+		var score = noodles.children.length;
+		var txt;
+		if(score == 0){
+			txt = game.add.text(width*0.5, height*0.5, "You cleared all the Macaronis!", {fontSize: "40px"});
+		}
+		else{
+			txt = game.add.text(width*0.5, height*0.5, "Macaronis left: "+score, {fontSize: "40px"});
+		}
 		txt.anchor.set(0.5);
 		game.paused = true;
 	}
